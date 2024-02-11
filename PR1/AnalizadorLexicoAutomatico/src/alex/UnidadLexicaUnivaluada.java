8efe1374a5882a698c4ae7da166a00428396663a
DannyP39 <danipiza@ucm.es>
2024-02-11 16:32:59 +0100

    PR1 Automatico print DOMJUDGE

--- PR1/AnalizadorLexicoAutomatico/src/alex/UnidadLexicaUnivaluada.java
+++ PR1/AnalizadorLexicoAutomatico/src/alex/UnidadLexicaUnivaluada.java
@@ -9,6 +9,7 @@ public class UnidadLexicaUnivaluada extends UnidadLexica {
 	
 	public String lexema() {throw new UnsupportedOperationException();}   
 	public String toString() {
-		return "[clase:"+clase()+",fila:"+fila()+",col:"+columna()+"]";  
+		//return "[clase:"+clase()+",fila:"+fila()+",col:"+columna()+"]";  
+		return super.print();
 	}   
 }
