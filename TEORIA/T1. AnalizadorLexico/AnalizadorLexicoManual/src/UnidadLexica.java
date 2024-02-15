906e98791e673a4558188e4d9a288141d6b8c98d
DannyP39 <danipiza@ucm.es>
2024-02-15 12:45:46 +0100

    PR1 Juez Lexer (Evalua teoria)

--- TEORIA/T1. AnalizadorLexico/AnalizadorLexicoManual/src/UnidadLexica.java
+++ TEORIA/T1. AnalizadorLexico/AnalizadorLexicoManual/src/UnidadLexica.java
@@ -1,14 +1,52 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public abstract class UnidadLexica {
 	
 	private ClaseLexica clase;
 	private int fila;
 	private int columna;
+	private Map<ClaseLexica, String> hashMap;
    
 	public UnidadLexica(int fila, int columna, ClaseLexica clase) {
 		this.fila = fila;
 		this.columna = columna;
 		this.clase = clase;
+		init();
+	}
+	
+	private void init() {
+		hashMap = new HashMap<>();
+		hashMap.put(ClaseLexica.PAP, "(");
+		hashMap.put(ClaseLexica.PCIERRE, ")");
+		hashMap.put(ClaseLexica.IGUAL, "=");		
+		hashMap.put(ClaseLexica.MAS, "+");
+		hashMap.put(ClaseLexica.MENOS, "-");
+		hashMap.put(ClaseLexica.POR, "*");
+		hashMap.put(ClaseLexica.DIV, "/");
+		hashMap.put(ClaseLexica.COMA, ",");
+		hashMap.put(ClaseLexica.EOF, "EOF");
+		
+		
+		
+	
+		//hashMap.put(ClaseLexica.ENT, "<int>");
+		//hashMap.put(ClaseLexica.REAL, "<real>");
+		hashMap.put(ClaseLexica.EVALUA, "<evalua>");
+		hashMap.put(ClaseLexica.DONDE, "<donde>");
+		
+		
+		
+		
+		/*IDEN
+		  
+		 SEP, E,
+		
+		TRUE, FALSE, AND, OR, NOT, BOOL,ENT, REAL*/
+	}
+	
+	public String print() {
+		return hashMap.get(clase);
 	}
    
 	public ClaseLexica clase () { return clase; }   
--- TEORIA/T1. AnalizadorLexico/AnalizadorLexicoManual/src/UnidadLexicaUnivaluada.java
+++ TEORIA/T1. AnalizadorLexico/AnalizadorLexicoManual/src/UnidadLexicaUnivaluada.java
@@ -14,7 +14,9 @@ public class UnidadLexicaUnivaluada extends UnidadLexica {
 	public String lexema() { throw new UnsupportedOperationException(); }
 	
 	public String toString() {
-		return "[clase:"+clase()+",fila:"+fila()+",col:"+columna()+"]";  
+		return super.print();
+		//return "[clase:"+clase()+",fila:"+fila()+",col:"+columna()+"]";  
 		//return "" + clase(); // DOMJUDGE
 	}   
+	
 }
