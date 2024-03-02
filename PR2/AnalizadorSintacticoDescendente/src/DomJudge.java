66840b66e00d874489ff851bf9184a10351343d3
DannyP39 <danipiza@ucm.es>
2024-03-02 19:57:52 +0100

    PR2 javacc descendente

--- PR2/AnalizadorSintacticoDescendente/src/DomJudge.java
+++ PR2/AnalizadorSintacticoDescendente/src/DomJudge.java
@@ -2,24 +2,28 @@ import asint.AnalizadorSintacticoTiny;
 import asint.AnalizadorSintacticoTinyDJ;
 import asint.ParseException;
 import asint.TokenMgrError;
+import errors.GestionErroresEval.ErrorLexico;
+import errors.GestionErroresEval.ErrorSintactico;
 
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
+
 public class DomJudge{
-   public static void main(String[] args) throws Exception {
-     try{  
-    	 Reader input = new InputStreamReader(new FileInputStream("input.txt"));
-    	 //Reader input = new InputStreamReader(System.in);
-    	 AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTinyDJ(input);
-    	 asint.analiza();
-     }
-     catch(ParseException e) {
-        System.out.println("ERROR_SINTACTICO"); 
-     }
-     catch(TokenMgrError e) {
-        System.out.println("ERROR_LEXICO"); 
-     }
-   }
+	
+	
+	public static void main(String[] args) throws Exception {
+		try{  
+			//Reader input = new InputStreamReader(new FileInputStream("input.txt"));
+			Reader input = new InputStreamReader(System.in);
+			AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTinyDJ(input);
+			asint.programa();
+		}
+		catch(ParseException e) {
+			System.out.println("ERROR_SINTACTICO"); 
+		}
+		catch(TokenMgrError e) {
+			System.out.println("ERROR_LEXICO"); 
+		}
+	}
 }
