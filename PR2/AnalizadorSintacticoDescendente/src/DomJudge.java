380ebe0f851c7e2d90646e7b5c6e378826f94585
DannyP39 <danipiza@ucm.es>
2024-03-02 16:47:04 +0100

    PR2 javacc

---
+++ PR2/AnalizadorSintacticoDescendente/src/DomJudge.java
@@ -0,0 +1,25 @@
+import asint.AnalizadorSintacticoTiny;
+import asint.AnalizadorSintacticoTinyDJ;
+import asint.ParseException;
+import asint.TokenMgrError;
+
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+public class DomJudge{
+   public static void main(String[] args) throws Exception {
+     try{  
+    	 Reader input = new InputStreamReader(new FileInputStream("input.txt"));
+    	 //Reader input = new InputStreamReader(System.in);
+    	 AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTinyDJ(input);
+    	 asint.analiza();
+     }
+     catch(ParseException e) {
+        System.out.println("ERROR_SINTACTICO"); 
+     }
+     catch(TokenMgrError e) {
+        System.out.println("ERROR_LEXICO"); 
+     }
+   }
+}
