cf2d4585c03b24ae3a806fa5f4b4fdde6622717b
Suhuai <suhuaichen2024@u.northwestern.edu>
2024-04-11 23:03:49 +0200

    solved some duplicate errors after merge(shouldn't use rebase). Deleted jflex.jar and cup.jar because they are not needed and will exceed file size constraint on DomJudge

--- PR3/G20/src/DomJudge.java
+++ PR3/G20/src/DomJudge.java
@@ -1,12 +1,10 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.IOException;
 import java.io.Reader;
 
 import asint.Impresion;
 import asint.SintaxisAbstractaTiny.Prog;
 
-import java.io.BufferedReader;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
@@ -16,25 +14,22 @@ import c_ast_ascendente.AnalizadorLexicoTiny;
 import c_ast_ascendente.AnalizadorSintacticoTinyDJAsc;
 import c_ast_ascendente.ALexOperations.ECaracterInesperado;
 import c_ast_ascendente.GestionErroresTiny.ErrorSintactico;
-import c_ast_ascendente.ALexOperations.ECaracterInesperado;
-import c_ast_ascendente.GestionErroresTiny.ErrorSintactico;
 //import c_ast_ascendente.ConstructorAST;
 import c_ast_descendente.AnalizadorSintacticoTinyDJ;
 // import c_ast_descendente.ConstructorASTsTiny;
 import c_ast_descendente.ParseException;
-import c_ast_descendente.TokenMgrError;
 import c_ast_descendente.TokenMgrError;
 
 public class DomJudge {
 
 	public static void main(String[] args) throws Exception  {
 		
-		// Reader input = new InputStreamReader(new FileInputStream("input.txt"));
+		//Reader input = new InputStreamReader(new FileInputStream("input.txt"));
 		
 		Reader input = new InputStreamReader(System.in);	
 		BufferedReader bufferedReader = new BufferedReader(input);
         
-        
+         
 		int parser = bufferedReader.read();
 		//System.out.println(parser);
 		//ConstructorAST asint = null;
@@ -49,17 +44,7 @@ public class DomJudge {
 				Prog prog =(Prog) asint.debug_parse().value;
 				// asint.debug_parse();
 				// Prog prog = (Prog)asint.parse().value;				
-				// Prog prog = (Prog)asint.parse().value;				
 				
-				System.out.println("IMPRESION RECURSIVA");
-				System.out.print(prog);
-				
-				System.out.println("IMPRESION INTERPRETE");
-				prog.imprime();
-				
-				System.out.println("IMPRESION VISITANTE");					
-				prog.procesa(new Impresion());
-					
 				System.out.println("IMPRESION RECURSIVA");
 				System.out.print(prog);
 				
@@ -75,35 +60,7 @@ public class DomJudge {
 			else {
 				System.out.println("CONSTRUCCION AST DESCENDENTE");
 				AnalizadorSintacticoTinyDJ asint = new AnalizadorSintacticoTinyDJ(bufferedReader);
-				System.out.println("CONSTRUCCION AST DESCENDENTE");
-				AnalizadorSintacticoTinyDJ asint = new AnalizadorSintacticoTinyDJ(bufferedReader);
 	            asint.disable_tracing();
-	            Prog prog=asint.inicial();
-	            
-				System.out.println("IMPRESION RECURSIVA");
-				System.out.print(prog);
-				
-				System.out.println("IMPRESION INTERPRETE");
-				prog.imprime();
-				
-				System.out.println("IMPRESION VISITANTE");					
-				prog.procesa(new Impresion());					   	            
-			}			
-			
-			
-		} 
-		catch(TokenMgrError e) {
-			System.out.println("ERROR_LEXICO"); 
-		}		
-		catch(ECaracterInesperado e2) {
-			System.out.println("ERROR_LEXICO"); 
-		}		
-		catch(ParseException e) {
-			System.out.println("ERROR_SINTACTICO"); 
-		}
-		catch(ErrorSintactico e) {
-			System.out.println("ERROR_SINTACTICO"); 
-		}
 	            Prog prog=asint.inicial();
 	            
 				System.out.println("IMPRESION RECURSIVA");
